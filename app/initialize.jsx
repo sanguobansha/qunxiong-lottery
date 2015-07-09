0f9e6b6650ce7fdcc2b78d71bb789fa3653d9c21
yct21 <yct21@12tcy.com>
2015-07-09 12:16:39 +0800

    clean up

--- app/initialize.jsx
+++
@@ -1,7 +0,0 @@
-var routes = require('routes');
-
-document.addEventListener('DOMContentLoaded', function() {
-  ReactRouter.run(routes, ReactRouter.HashLocation, function(Root) {
-    React.render(<Root/>, document.body);
-  });
-}, false);
